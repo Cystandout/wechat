d6c3a9c8e98b2460d571099b9ea0a25b447cdb66
刘杰 <2936236899@qq.com>
2017-08-22 20:55:15 +0800

    快递查询

---
+++ app.js
@@ -0,0 +1,44 @@
+//app.js
+App({
+  onLaunch: function () {
+    //调用API从本地缓存中获取数据
+    var logs = wx.getStorageSync('logs') || []
+    logs.unshift(Date.now())
+    wx.setStorageSync('logs', logs)
+  },
+
+  getUserInfo: function (cb) {
+    var that = this
+    if (this.globalData.userInfo) {
+      typeof cb == "function" && cb(this.globalData.userInfo)
+    } else {
+      //调用登录接口
+      wx.getUserInfo({
+        withCredentials: false,
+        success: function (res) {
+          that.globalData.userInfo = res.userInfo
+          typeof cb == "function" && cb(that.globalData.userInfo)
+        }
+      })
+    }
+  },
+  getExpressInfo: function (nu, cb) {
+    wx.request({
+      url: 'http://apis.baidu.com/kuaidicom/express_api/express_api?muti=0&order=desc&nu='+nu, //仅为示例，并非真实的接口地址
+      data: {
+        x: '',
+        y: ''
+      },
+      header: {
+        'apikey': '05cc731d685008533233542738b4687d'
+      },
+      success: function (res) {
+        // console.log(res.data)
+        cb(res.data);
+      }
+    })
+  },
+  globalData: {
+    userInfo: null
+  }
+})
